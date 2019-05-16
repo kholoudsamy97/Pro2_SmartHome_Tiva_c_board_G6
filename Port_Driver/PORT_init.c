71bea4f0080b4c3be4d6b2a80eb486faa183be30
SaraSafwat <sara.safwat02@gmail.com>
2019-05-17 00:50:25 +0200

    PORT_Init

---
+++ Port_Driver/PORT_init.c
@@ -0,0 +1,27 @@
+#include "PORT.h"
+
+void port_Init( uint8_t port_index)
+{	
+	SYSCTL_RCGCGPIO_R	|= 1<<port_index ; // Clock = 0000 0001 = PORT_ enable
+	while((SYSCTL_PRGPIO_R & (1<<port_index)) == 0);//0x40005520
+	
+	
+	if( port_index <= 3)
+	{
+		(*(volatile uint32_t *)( (port_index * 0x00001000 ) + 0x40004520)) = 0x4C4F434B;//UNLOCK
+		(*(volatile uint32_t *)( (port_index * 0x00001000 ) + 0x40004524)) = 0x1F ;//CR
+		(*(volatile uint32_t *)( (port_index * 0x00001000 ) + 0x4000451C)) = 0x1F;//DEN
+		(*(volatile uint32_t *)( (port_index * 0x00001000 ) + 0x40004420)) = 0 ;//AFSEL
+		(*(volatile uint32_t *)( (port_index * 0x00001000 ) + 0x40004528)) = 0 ;//AMSEL_R
+		(*(volatile uint32_t *)( (port_index * 0x00001000 ) + 0x4000452C)) = 0 ;//PCTL
+	}
+	else 
+	{                                                 
+		(*(volatile uint32_t *)( ((port_index-4) * 0x00001000) + 0x40024520)) = 0x4C4F434B;//UNLOCK
+		(*(volatile uint32_t *)( ((port_index-4) * 0x00001000) + 0x40024524)) = 0x1F ;//CR
+		(*(volatile uint32_t *)( ((port_index-4) * 0x00001000) + 0x4002451C)) = 0x1F;//DEN
+		(*(volatile uint32_t *)( ((port_index-4) * 0x00001000) + 0x40024420)) = 0 ;//AFSEL
+		(*(volatile uint32_t *)( ((port_index-4) * 0x00001000) + 0x40024528)) = 0 ;//AMSEL_R
+		(*(volatile uint32_t *)( ((port_index-4) * 0x00001000) + 0x4002452C)) = 0 ;//PCTL
+	}
+}
